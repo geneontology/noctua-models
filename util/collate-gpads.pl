68cfc5a55aa1db573f4e313f909f392ba441a7b0
obophenotype <ontologywizard@example.org>
2018-03-19 16:51:54 -0700

    adding header and other metadata for collation, see https://github.com/geneontology/go-site/issues/431

--- util/collate-gpads.pl
+++ util/collate-gpads.pl
@@ -2,6 +2,9 @@
 use strict;
 use FileHandle;
 
+my $today = `date +%Y-%m-%d`;
+chomp $today;
+
 # maps group names to filehandles;
 # e.g. zfin => wrte filehandle for zfin.gpad
 my %fhmap = ();
@@ -25,12 +28,18 @@ sub write_line_to_file {
     my @props = split(/\|/, $colvals[11]);
     if (!grep {$_ eq 'model-state=production'} @props) {
         # skip non-production models
+        print STDERR "No production models for $base\n";
         return;
     }
     $base = lc($base);
+    print STDERR "Writing: $base\n";
     if (!$fhmap{$base}) {
         my $fh = FileHandle::new();
         $fh->open(">legacy/$base.gpad") || die $base;
+        print $fh "!gpa-version: 1.2\n";
+        print $fh "!collation date: $today\n";
+        print $fh "!collated from production models in https://github.com/geneontology/noctua-models/ where col1 matches $base\n";
+
         $fhmap{$base} = $fh;
     }
     my $fh = $fhmap{$base};
