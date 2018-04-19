87c28377f28fdcb10b39a5a6bdb076947a1ecb86
cmungall <cjm@berkeleybop.org>
2018-04-18 17:10:45 -0700

    Migrate URIs, see #617

---
+++ util/fix-uris.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -np
+# xenbase OK
+# sgd OK
+# pombase OK
+s@http://www.informatics.jax.org/accession/MGI:MGI:@http://identifiers.org/mgi/MGI:@g;
+s@http://zfin.org/@http://identifiers.org/zfin/@g;
+s@http://rgd.mcw.edu/rgdweb/report/gene/main.html?id=@http://identifiers.org/rgd/@g;
+
+
+
+
+    
